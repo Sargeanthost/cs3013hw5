c1219e279330722ad461a83ea88c7996b6f022ab
girraiffe <sargeanthost@gmail.com>
2023-02-22 01:30:36 -0500

    valgrind errors, some bounds checking needs to be done

--- scan.c
+++ scan.c
@@ -1,11 +1,9 @@
 #include <assert.h>
 #include <stdio.h>
-#include <math.h>
 #include <string.h>
 #include <pthread.h>
 #include <semaphore.h>
 #include <stdlib.h>
-#include <string.h>
 
 #define MAX_LINE_SIZE 256
 
@@ -47,7 +45,6 @@ typedef struct {
 } thread_args_t;
 
 barrier_t read_barrier;
-barrier_t write_barrier;
 
 void *hillis_steele_scan(void *args) {
     thread_args_t *threadArgs = (thread_args_t *) args;
@@ -55,46 +52,25 @@ void *hillis_steele_scan(void *args) {
     int size = threadArgs->array_size;
     int index = threadArgs->index;
     int chunk = threadArgs->chunk;
-    printf("INDEX: %d\n", index);
-    printf("chunk: %d\n", chunk);
 
-    //2 threads -> chunk is 3
-    //[1,2,3,4,5,6]
-    //thread 1 manages all of 0-2
-
-    //acts as floor(log()) in conjunction with d<size
     int d = 1;
 
     while (d < size) {
         int offset = 1 << (d - 1);
         int old_values[size];
+        // Ensure in sync read so we dont read updated values
         memcpy(&old_values, array, sizeof(int) * size);
-        //old
-        printf("Old values: [");
-        for (int k = 0; k < size - 1; k++) {
-            printf("%d, ", old_values[k]);
-        };
-        printf("%d]\n", old_values[size - 1]);
-//        synchronous read
         barrier_wait(&read_barrier);
-        for (int i = index; i < index + chunk; i++) {
 
-            printf("i + offset %d\n", (i + offset));
-//            if (i + offset < size) {
+        //TODO bounds check these things
+        for (int i = index; i < threadArgs->index + chunk; i++) {
+            if (index + offset < size) {
                 array[i + offset] += old_values[i];
-//                puts("HIT IF STATEMENT");
-//            }
-            printf("Print from scan func: [");
-            for (int k = 0; k < size - 1; k++) {
-                printf("%d, ", array[k]);
-            };
-            printf("%d]\n", array[size - 1]);
+            }
         }
-// Synchronize threads
-//        barrier_wait(&write_barrier);
-
         d *= 2;
     }
+    return NULL;
 }
 
 void read_input_vector(const char *filename, int *array) {
@@ -102,6 +78,7 @@ void read_input_vector(const char *filename, int *array) {
     char *line = malloc(MAX_LINE_SIZE + 1);
     size_t len = MAX_LINE_SIZE;
 
+    //ctrl+d to EOF
     fp = strcmp(filename, "-") ? fopen(filename, "r") : stdin;
 
     assert(fp != NULL && line != NULL);
@@ -131,25 +108,16 @@ int main(int argc, char **argv) {
 
     read_input_vector(file_name, input_array); //assign vectors
 
-    printf("Before: [");
-    for (int i = 0; i < vector_size - 1; i++) {
-        printf("%d, ", input_array[i]);
-    };
-    printf("%d]\n", input_array[vector_size - 1]);
-
     pthread_t *threads = malloc(num_threads * sizeof(pthread_t));
     //parallel array for use with the matching thread. keeps track of the threads accessible threads, the range of items
     //to work on, and the array
     thread_args_t *thread_args = malloc(num_threads * sizeof(thread_args_t));
-    //init barrier
+
     barrier_init(&read_barrier, num_threads);
-    barrier_init(&write_barrier, num_threads);
 
     //assumes you wont give it more threads than the size of the array
     int chunk = vector_size / num_threads;
 
-    //init thread args
-    //flip this and keep one barrier?
     for (int i = 0; i < num_threads; i++) {
         thread_args[i].array = input_array;
         thread_args[i].array_size = vector_size;
@@ -157,16 +125,15 @@ int main(int argc, char **argv) {
         thread_args[i].chunk = chunk;
         pthread_create(&threads[i], NULL, hillis_steele_scan, &thread_args[i]);
     }
-    //wait on threads so we dont die before finishing
+    //wait on threads so we dont die before finishing TODO do we even need this?
     for (int i = 0; i < num_threads; i++) {
         pthread_join(threads[i], NULL);
     }
 
-    printf("After: [");
-    for (int i = 0; i < vector_size - 1; i++) {
-        printf("%d, ", input_array[i]);
-    };
-    printf("%d]\n", input_array[vector_size - 1]);
+    //print out final array
+    for (int i = 0; i < vector_size; i++) {
+        printf("%d\n", input_array[i]);
+    }
 
     free(input_array);
     free(threads);
